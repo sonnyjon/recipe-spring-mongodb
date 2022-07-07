2fab812e223f9f29e31dbe28d3ec068d7a97f37d
sonnyjon <sonny.s.jon@gmail.com>
2022-07-07 16:54:50 -0700

    first commit

---
+++ src/test/java/dev/sonnyjon/recipespringmongodb/RecipeSpringMongodbApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.sonnyjon.recipespringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringMongodbApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
